bd7bbf3fbba8c3d1acfdacc2551ec0d6954ab1dc
derek-teaney <derek.teaney@stonybrook.edu>
2021-12-17 23:27:28 -0500

    program to read ntuple

--- ModelG/ntuple.h
+++ ModelG/ntuple.h
@@ -3,11 +3,11 @@
 
 #include "hdf5.h"
 #include <array>
-#include <vector>
+#include <iostream>
 #include <string>
+#include <vector>
 
-
-/*  This gives the basic usage 
+/*  This gives the basic usage
 int main(int argc, char **argv) {
 
   hid_t file_id;
@@ -46,55 +46,80 @@ int main(int argc, char **argv) {
 }
 */
 
-
-template<std::size_t rank>
-class ntuple {
+template <std::size_t rank> class ntuple {
 
 public:
-  std::vector<double> row ;
+  std::vector<double> row;
+
+  std::array<size_t, rank + 1> get_dims() {
+    std::array<size_t, rank + 1> Dims;
+    for (size_t i = 0; i < rank + 1; i++) {
+      Dims[i] = dims[i];
+    }
+    return Dims;
+  }
+
+  std::array<size_t, rank> getN() {
+    std::array<size_t, rank> NN;
+    for (size_t i = 0; i < rank; i++) {
+      NN[i] = N[i];
+    }
+    return NN;
+  }
+
+  void getN(std::array<size_t, rank> &NN) {
+    for (size_t i = 0; i < rank; i++)
+      NN[i] = N[i];
+  }
+  size_t nrows() { return static_cast<size_t>(dims[0]); }
+
+  void readrow(const size_t &irow) {
+    // Get the space in the file
+    hid_t filespace = H5Dget_space(dataset);
+
+    // set the offset and count
+    hsize_t offset[rank + 1] = {};
+    offset[0] = irow;
+    hsize_t count[rank + 1];
+    pack(count, 1, N);
+
+    // Get the hyperslab
+    herr_t status = H5Sselect_hyperslab(filespace, H5S_SELECT_SET, offset, NULL,
+                                        count, NULL);
+
+    // Write the data
+    status = H5Dread(dataset, H5T_NATIVE_DOUBLE, memory_space, filespace,
+                     H5P_DEFAULT, row.data());
+
+    // Close the filespace
+    H5Sclose(filespace);
+  }
 
   size_t at(const std::array<int, rank> &idex) {
     size_t offset = 0;
-    for(size_t i=0; i < rank; i++) offset += sizes[i]*idex[i] ;
-    return offset ;
+    for (size_t i = 0; i < rank; i++)
+      offset += sizes[i] * idex[i];
+    return offset;
+  }
+
+  ntuple(const std::string &nm, const hid_t &file_id) : closed(false) {
+    init({}, nm, file_id, false);
   }
 
   ntuple(const std::array<size_t, rank> &n, const std::string &nm,
-         const hid_t &file_id) : closed(false) {
-
-    size_t count =1 ;
-    for (size_t i = 0; i < rank; i++) {
-      N[i] = n[i];
-      count *= n[i] ;
-    }
-    row.resize(count) ;
-
-    sizes[rank-1] = 1 ;
-    // gotcha: use int not size_t can go negative
-    for (int i = static_cast<int>(rank)-2; i >=0; i--) {
-       sizes[i] = N[i+1]*sizes[i+1] ;
-    }
-    
-    // Open the dataset
-    htri_t exists = H5Lexists(file_id, nm.c_str(), H5L_TYPE_HARD) ;
-    if (!exists) {
-      create(file_id, nm) ;
-    } else {
-      open(file_id, nm) ;
-    }
-
-    // Memory of space
-    hsize_t line_dims[rank+1];
-    pack(line_dims, 1, N);
-    memory_space = H5Screate_simple(rank+1, line_dims, NULL);
+         const hid_t &file_id)
+      : closed(false) {
+    init(n, nm, file_id, true);
   }
 
   ~ntuple() {
-    if (!closed) close();
+    if (!closed)
+      close();
   }
 
   void close() {
-    if (closed) return ;
+    if (closed)
+      return;
 
     // Close the filespace
     H5Sclose(memory_space);
@@ -105,7 +130,7 @@ public:
     // and the dataspace
     H5Sclose(dataspace);
 
-    closed = true ;
+    closed = true;
   }
 
   void fill() {
@@ -117,9 +142,9 @@ public:
     hid_t filespace = H5Dget_space(dataset);
 
     // set the offset and count
-    hsize_t offset[rank+1] = {};
+    hsize_t offset[rank + 1] = {};
     offset[0] = dims[0] - 1;
-    hsize_t count[rank+1];
+    hsize_t count[rank + 1];
     pack(count, 1, N);
 
     // Get the hyperslab
@@ -135,9 +160,10 @@ public:
   }
 
 private:
-
   // number of columns
   hsize_t N[rank];
+  // Sizes of the array for a (2,3,4) array this is (12,4,1), see at
+  size_t sizes[rank];
 
   // memory
   bool closed;
@@ -146,32 +172,30 @@ private:
   hid_t memory_space;
 
   // dimensions of the stored array
-  hsize_t dims[rank+1];
+  hsize_t dims[rank + 1];
 
   // helper
   void pack(hsize_t d[], hsize_t d0, hsize_t n[]) {
-    d[0] = d0 ;
-    for (size_t i = 0; i< rank; i++) {
-      d[i+1] = n[i] ;
+    d[0] = d0;
+    for (size_t i = 0; i < rank; i++) {
+      d[i + 1] = n[i];
     }
   }
 
-  size_t sizes[rank] ;
-
-  void create(hid_t file_id, const std::string &nm)  {
+  void create(hid_t file_id, const std::string &nm) {
     // Set the  array dimensions
     pack(dims, 0, N);
-    hsize_t maxdims[rank+1];
+    hsize_t maxdims[rank + 1];
     pack(maxdims, H5S_UNLIMITED, N);
 
     // Create the dataspace
-    dataspace = H5Screate_simple(rank+1, dims, maxdims);
+    dataspace = H5Screate_simple(rank + 1, dims, maxdims);
 
     // set chunking properties of the dataset
     hid_t chunk_params = H5Pcreate(H5P_DATASET_CREATE);
-    hsize_t chunk_dims[rank+1];
+    hsize_t chunk_dims[rank + 1];
     pack(chunk_dims, 2, N);
-    herr_t status = H5Pset_chunk(chunk_params, rank+1, chunk_dims);
+    herr_t status = H5Pset_chunk(chunk_params, rank + 1, chunk_dims);
 
     // Open the dataset
     dataset = H5Dcreate(file_id, nm.c_str(), H5T_NATIVE_DOUBLE, dataspace,
@@ -180,23 +204,67 @@ private:
     H5Pclose(chunk_params);
   }
 
-  void open(hid_t file_id, const std::string &nm) {
+  // Open an existing dataset in the file. If checkdims is true, check that the
+  // dimensioning of that set agrees with the dimension stored in N.
+  void open(hid_t file_id, const std::string &dset, const bool &checkdims) {
 
-    dataset = H5Dopen(file_id,  nm.c_str(), H5P_DEFAULT) ;
-    dataspace = H5Dget_space(dataset);    
+    dataset = H5Dopen(file_id, dset.c_str(), H5P_DEFAULT);
+    dataspace = H5Dget_space(dataset);
 
     // This line reads the dimensions of the array from tape
-    hsize_t maxdims[rank+1] ;
-    herr_t status  = H5Sget_simple_extent_dims(dataspace, dims, maxdims);
+    hsize_t maxdims[rank + 1];
+    herr_t status = H5Sget_simple_extent_dims(dataspace, dims, maxdims);
 
-    // check that the dimensions agree
-    for (size_t i=0; i < rank; i++) {
-      if (N[i] != dims[i+1]) {
-        throw ("ntuple::ntuple Error dataset dimensions do not conform") ;
+    // check that the dimensions agree or set the dimension array
+    for (size_t i = 0; i < rank; i++) {
+      if (checkdims) {
+        if (N[i] != dims[i + 1]) {
+          throw("ntuple::ntuple Error dataset dimensions do not conform");
+        }
+      } else {
+        N[i] = dims[i + 1];
       }
     }
   }
-  
+
+  void init(const std::array<size_t, rank> &n, const std::string &dset,
+            const hid_t &file_id, const bool &checkdims = true) {
+
+    for (size_t i = 0; i < rank; i++) {
+      N[i] = n[i];
+    }
+
+    // Open the dataset
+    htri_t exists = H5Lexists(file_id, dset.c_str(), H5L_TYPE_HARD);
+    if (!exists) {
+      create(file_id, dset);
+    } else {
+      open(file_id, dset, checkdims);
+    }
+
+    // Allocate the buffer
+    size_t count = 1;
+    for (size_t i = 0; i < rank; i++) {
+      count *= N[i];
+    }
+    if (count > 0) {
+      row.resize(count);
+    } else {
+      throw("ntuple::ntuple Error in dimensioning array: count is zero.");
+    }
+
+    // Initialize the sizes array
+    sizes[rank - 1] = 1;
+    // gotcha: use int not size_t can go negative
+    for (int i = static_cast<int>(rank) - 2; i >= 0; i--) {
+      sizes[i] = N[i + 1] * sizes[i + 1];
+    }
+
+    // Memory of space
+    hsize_t line_dims[rank + 1];
+    pack(line_dims, 1, N);
+    memory_space = H5Screate_simple(rank + 1, line_dims, NULL);
+  }
 };
 
 #endif
--- ModelG/test_ntuple.cxx
+++ ModelG/test_ntuple.cxx
@@ -10,48 +10,111 @@ int main(int argc, char **argv) {
   static const hsize_t N = 8;
   std::array<double, N> datain = {1, 2, 3, 4, 5, 6, 7, 8};
 
-  std::array<size_t, 1> NN1 = {8} ;
+  std::array<size_t, 1> NN1 = {8};
   ntuple<1> nt1(NN1, "phi1", file_id);
 
-  std::array<size_t, 2> NN2 = {2,4} ;
+  std::array<size_t, 2> NN2 = {2, 4};
   ntuple<2> nt2(NN2, "phi2", file_id);
 
-  std::array<size_t, 3> NN3 = {2,2,2 } ;
+  std::array<size_t, 3> NN3 = {2, 2, 2};
   ntuple<3> nt3(NN3, "phi3", file_id);
 
   for (size_t j = 0; j < 3; j++) {
 
-    for (int i0=0; i0 < NN1[0] ; i0++) {
+    for (int i0 = 0; i0 < NN1[0]; i0++) {
       // Seems a bit excessive in 1d
-      //size_t k = nt.at({i0, i1}) ;
-      nt1.row[i0] = datain[i0] ;
+      // size_t k = nt.at({i0, i1}) ;
+      nt1.row[i0] = datain[i0];
     }
 
-    for (int i0=0; i0 < NN2[0] ; i0++) {
-    for (int i1=0; i1 < NN2[1] ; i1++) {
-      size_t k = nt2.at({i0, i1}) ;
-      nt2.row[k] = datain[k] ;
-    }
+    for (int i0 = 0; i0 < NN2[0]; i0++) {
+      for (int i1 = 0; i1 < NN2[1]; i1++) {
+        size_t k = nt2.at({i0, i1});
+        nt2.row[k] = datain[k];
+      }
     }
 
-    for (int i0=0; i0 < NN3[0] ; i0++) {
-    for (int i1=0; i1 < NN3[1] ; i1++) {
-    for (int i2=0; i2 < NN3[1] ; i2++) {
-      size_t k = nt3.at({i0, i1,i2}) ;
-      nt3.row[k] = datain[k] ;
-    }
-    }
+    for (int i0 = 0; i0 < NN3[0]; i0++) {
+      for (int i1 = 0; i1 < NN3[1]; i1++) {
+        for (int i2 = 0; i2 < NN3[2]; i2++) {
+          size_t k = nt3.at({i0, i1, i2});
+          nt3.row[k] = datain[k];
+        }
+      }
     }
     nt1.fill();
     nt2.fill();
     nt3.fill();
-
   }
 
   // Close the ntuple
   nt1.close();
   nt2.close();
   nt3.close();
+
+  // close the filespace
+  H5Fclose(file_id);
+
+  // Open the filespace and grab the ntuples
+  file_id = H5Fopen("work.h5", H5F_ACC_RDONLY, H5P_DEFAULT);
+  ntuple<1> in1("phi1", file_id);
+  ntuple<2> in2("phi2", file_id);
+  ntuple<3> in3("phi3", file_id);
+
+  // Get the dimensioning
+  auto IN1 = in1.getN();
+  auto IN2 = in2.getN();
+  auto IN3 = in3.getN();
+
+  auto D2 = in2.get_dims();
+  printf("The dimensions are: ");
+  for (size_t i = 0; i < 3; i++) {
+    printf("%lu ", (D2[i]));
+  }
+  printf("\n");
+
+  printf("Ntumple dimensions are : ");
+  for (size_t i = 0; i < 2; i++) {
+    printf("%lu ", IN2[i]);
+  }
+  printf("\n");
+
+  for (size_t i = 0; i < in1.nrows(); i++) {
+    in1.readrow(i);
+    for (size_t j = 0; j < IN1[0]; j++) {
+      printf("%15.5e ", in1.row[j]);
+    }
+    printf("\n");
+
+    printf("\n");
+    in2.readrow(i);
+    for (int i0 = 0; i0 < IN2[0]; i0++) {
+      for (int i1 = 0; i1 < IN2[1]; i1++) {
+        size_t k = nt2.at({i0, i1});
+        printf("%15.5e ", in2.row[k]);
+      }
+      printf("\n");
+    }
+    printf("\n");
+
+    printf("\n");
+    in3.readrow(i);
+    for (int i0 = 0; i0 < IN3[0]; i0++) {
+      for (int i1 = 0; i1 < IN3[1]; i1++) {
+        for (int i2 = 0; i2 < IN3[2]; i2++) {
+          size_t k = nt3.at({i0, i1, i2});
+          printf("%15.5e ", in3.row[k]);
+        }
+        printf("\n");
+      }
+      printf("\n");
+    }
+    printf("\n");
+  }
+
+  in1.close();
+  in2.close();
+  in3.close();
 
   // close the filespace
   H5Fclose(file_id);
